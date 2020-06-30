0a6810680e348a375e0708089ce05aaeea98b64f
usac201701026 <3003552890101@ingenieria.usac.edu.gt>
2020-06-29 20:34:47 -0600

    casi final

--- final/servidor.py
+++ final/servidor.py
@@ -1,3 +1,6 @@
+#Jose Fernando Marticorena Barrientos
+#
+
 import paho.mqtt.client as mqtt
 import logging
 import time
@@ -20,116 +23,116 @@ carne=''
 inicio =0
 
 
-class Comandos():
+class Comandos():                              #CJFMBlase de Comandos
     def __init__(self, comando):
-        self.comando = comando
+        self.comando = comando              
         #self.detectar(l1)
 
-    def detectar(self,comando,client,topic):
-        if comando[3:6] == 'x04': 
+    def detectar(self,comando,client,topic):    #JFMB Metodo para ver que comando llega
+        if comando[3:6] == 'x04':               #JFMB Si LLega el comando x04 Alive 
             carne = comando[6:15]
-            self.Alive(carne,client) 
-        if comando[3:6] == 'x03':
+            self.Alive(carne,client)            #JFMB Nos conduce al metodo de usuarios activos con el argumento del id o carne del usuario
+        if comando[3:6] == 'x03':               #JFMB si llema el comando x03 es el request de envio
             destino = comando.split('$')[1]
             tamaño = comando.split('$')[2]
             emisor = topic[12:]
-            self.FTR(destino,tamaño,emisor,client)
+            self.FTR(destino,tamaño,emisor,client) #JFMB nos dirije a metoroFRT con destino, tama;o de arch y el emisor como argumentos
             print(self.vec)
 
-    def FTR(self,destino,tamaño,emisor,client):
-        destinos=[]                     #para almacenar los usuarios en salas
-        receptores = []                 #para almacenar a quiener se les mandara FRR
+    def FTR(self,destino,tamaño,emisor,client): #JFMB metodo FTR
+        destinos=[]                     #JFMB para almacenar los usuarios en salas
+        receptores = []                 #JFMB para almacenar a quiener se les mandara FRR
         if (len(destino)<=6):
-            f = open("salasser","r")               
-            while(True):
+            f = open("salasser","r")        #JFMB utilizamos el archivo salaser para ver quiene estan en la sala        
+            while(True):                    #JFMB while para leer el archivo
                 linea = f.readline()
                 info = linea.split(',')
                 if(destino in info):
-                    destinos.append(info[0])
+                    destinos.append(info[0])    #JFMB guardamos los usuarips de la sala
                 if not linea:
                     break    
             f.close()
             cont = 0
             for i in range(len(destinos)):
-                if destinos[i] in self.vec:
-                    print('FTR Es en sala activo')
-                    receptores.append(destinos[i])
+                if destinos[i] in self.vec:          # JFMB aca vemos si el usuario esta activo
+                    print('FTR Es en sala activo') 
+                    receptores.append(destinos[i])  #JFMB ingresamos los usr activos
                     cont=cont+1
             if cont>0 :
-               self.OK(emisor,client,receptores,tamaño)
+               self.OK(emisor,client,receptores,tamaño)   #JFMB si hay mas de 0 usuarios activos vamos al metodo OK
             else:
-                self.NO(emisor,client)
+                self.NO(emisor,client)      #JFMB sino al metodo No
 
 
-        else:
-            if destino in self.vec:
+        else:                               #sJFMB i solo se hizo el request para 1 usuario
+            if destino in self.vec:             #JFMB verificamos si esta activo
                 print('FTR Es usuario activo')
-                receptores.append(destino)
-                self.OK(emisor,client,receptores,tamaño)
+                receptores.append(destino)      #JFMB lo agregamos al cevtor de activos
+                self.OK(emisor,client,receptores,tamaño)  #si activo Ok  
             else:
-                self.NO(emisor,client)
+                self.NO(emisor,client)      #sino No
 
-    def NO(self,emisor,client):
+    def NO(self,emisor,client):         
         emisr = str(emisor).encode()
         No = b'\x07' + b'$' + emisr        
-        client.publish('comandos/22/'+ emisor, No)
+        client.publish('comandos/22/'+ emisor, No)  #JFMB publicamos al topic + el emisor del request el comando No
         print('NO fue publicado')
 
     def OK(self,emisor,client,receptores,tamaño):
         emisr = str(emisor).encode()
         Ok = b'\x06' + b'$' + emisr
         print(emisor)
-        client.publish('comandos/22/'+ emisor, Ok)
+        client.publish('comandos/22/'+ emisor, Ok)      #JFMB publicamos al topic + el emisor del request el comando Si
         print('Ok fue publicado')
-        self.recvaudio(client,receptores,emisor,tamaño) 
+        self.recvaudio(client,receptores,emisor,tamaño) #JFMB y vamos al metodo d recibir el audio
         
 
-    def Alive(self, usr,client):   
-        if usr in alives:
-            pos = alives.index(usr)
-            cont[pos] = cont[pos] + 1
+    def Alive(self, usr,client):   #JFMB metodo para marcar al usuario como alive
+        if usr in alives:           #JFMBsi el usuario ya esta en el vector alives
+            pos = alives.index(usr)     #vJFMB vemos en que posicion esta del vector
+            cont[pos] = cont[pos] + 1   #JFMB y en un contador agregamos que nos llego su alive cada 2seg
 
         else:
-            alives.append(usr)
-            cont.append(1)
+            alives.append(usr)      #JFMB si no esta en la lista lo agregamos
+            cont.append(1)      #JFMB y le ponemos 1 a su contador
             inicio=0
-
+#empieza el hilo infinito para usr activos
         def vivos():
             while True:
                 c=0 
-                while (c <= 3):
+                while (c <= 3):     #JFMB hacemos un ciclo infinito de 3 ciclos de 2 segundos(6seg)
                     c = c +1
                     #print(str(c))
                     time.sleep(2) 
-                if (c >= 3):
-                    for i in range(len(alives)):
-                        if(cont[i]==0):
-                            alives.pop(i)
+                if (c >= 3):                    #JFMb si en esos 6 seg
+                    for i in range(len(alives)):    #JFMB leemos todos los activos
+                        if(cont[i]==0):             #JFMB el contador del usuario es 0
+                            alives.pop(i)           #JFMB lo eliminamos y su contador tambien
                             cont.pop(i)
                             print('borrado')
                         else:
-                            cont[i]=0 
+                            cont[i]=0               #JFMB si el contador el mayor a 0 lo reseteamos y sigue vivo
                             print('reseteado')  
                     c=0  
-                    print(alives)  
+                    print(alives)               
                     print("estos son activos")
                     #client.publish('usuarios/22/201701026', 'Activo')
-                    self.ACK(alives,client)            
-        if (inicio == 0):          
+                    self.ACK(alives,client)   #JFMB vamos a AKC con la lista de activos         
+        if (inicio == 0):                               #llamamos al hilo solo al empezar
             vivo = threading.Thread(name = 'Alives', target = vivos,daemon = True)
                 #Luego de configurar cada hilo, se inicializan
             vivo.start()   
             self.inicio = 1
 
-    def ACK(self,activ,client):
+    def ACK(self,activ,client): 
         self.vec = activ
-        if (len(self.vec)):
+        if (len(self.vec)):                 #JFMB leemos la lista de activos
             for i in range(len(self.vec)):
                 idee = str(self.vec[i]).encode()
                 akc = b'\x05' + b'$' + idee
-                client.publish('comandos/22/'+self.vec[i], akc)
+                client.publish('comandos/22/'+self.vec[i], akc)     #JFMB y publicamos a cada uno de ellos el ACK
 
-    def recvaudio(self,client,receptores,emisor,tamaño):
+    def recvaudio(self,client,receptores,emisor,tamaño):        #JFMB metood para recibir audio
         def recibiraudio():
             server_socket = socket()
             server_socket.bind((SERVER_ADDR, SERVER_PORT))
@@ -156,7 +159,7 @@ class Comandos():
                             break
                         print("Se ha recibido el archivo")
                         print('cerrando servidor')
-                        server_socket.shutdown(SHUT_RDWR)
+                        
                         audior.close()
                         server_socket.close()
                         self.FRR(client,receptores,emisor,tamaño)
@@ -167,50 +170,44 @@ class Comandos():
         #Luego de configurar cada hilo, se inicializan
         au.start() 
 
-    def FRR(self,client,receptores,emisor,tamaño):
+    def FRR(self,client,receptores,emisor,tamaño):      #JFMB luego de recibir audio nos manda al FRR
         emisr = str(emisor).encode()
         tam = str(tamaño).encode()
         FRR = b'\x02' + b'$' + emisr + b'$' + tam
         print('Esto son los receptores'+ str(receptores))
         for i in range(len(receptores)):
-            client.publish('comandos/22/'+str(receptores[i]), FRR ) 
+            client.publish('comandos/22/'+str(receptores[i]), FRR ) #JFMB le publicamos a todos los receptores el FRR
         self.enviarAu()
 
-    def enviarAu(self):
+    def enviarAu(self):             #y nos vamos a enviar audio
         def envrau():
-            
             server_socket = socket()
             #server_socket.bind((SERVER_ADDR, SERVER_PORT))
-            server_socket.listen(100) #1 conexion activa y 9 en cola
-            print("\nEsperando conexion remota...\n")
-            conn, addr = server_socket.accept()
+            server_socket.listen(0) #1 conexion activa y 9 en cola
             try:
                 while True:
+                    print("\nEsperando conexion remota...\n")
+                    conn, addr = server_socket.accept()
                     print('Conexion establecida desde ', addr)
                     print('Enviando Audio...')
-
-                    audio = open('servi.wav', 'rb') 
-                    archivo = audio.read(64*1024)
-                    while archivo:
-                        conn.sendfile(audio)
-                        archivo = audio.read(64*1024)
-                    audio.close()
+                    with open('recibido.wav', 'rb') as audio: #Se abre el archivo a enviar en BINARIO
+                        conn.sendfile(audio, 0)
+                        audio.close()
                     conn.close()
                     print("\n\nArchivo enviado a: ", addr)
-                    break
             finally:
                 print("Cerrando el servidor...")
-                server_socket.close()        
+                server_socket.close()      
         envau = threading.Thread(name = 'audio hilo enviar', target = envrau,daemon = True)
         #Luego de configurar cada hilo, se inicializan
         envau.start()  
 
-class Servidor(Comandos):
+class Servidor(Comandos):               #JFMB clase de servidor
     def __init__(self, subs, destino):
         self.subs = subs
         self.destino = destino
 
-    def conecmqtt(self):
+    def conecmqtt(self):                    #JFMB Metodo para conectar al broker
         qos =2
         inicio = 0
         client = mqtt.Client(clean_session=True)
@@ -220,7 +217,7 @@ class Servidor(Comandos):
         client.subscribe([("comandos/22/201742012", qos), ("comandos/22/201701026", qos), ("comandos/22/201709532", qos), ("comandos/22", qos)])
         self.recibir(client)
 
-    def recibir(self,client):
+    def recibir(self,client):       #JFMB recibimos infinitamente mensajes por hilos
         self.c=0
         def recibir():
             while True:
